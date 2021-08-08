f0eabdfd42703eac5dee062a00096159d2991eca
partner <446729313@qq.com>
2021-08-08 11:44:47 +0800

    无

--- src/main/java/Leetcode13.java
+++ src/main/java/Leetcode13.java
@@ -14,11 +14,12 @@ public class Leetcode13 {
         m.put('C', 100);
         m.put('D', 500);
         m.put('M', 1000);
+        //符号对应数字一个一个，现场找规律有点难。
         Integer value=0;
         Integer result = 0;
         for (int i = 0; i <s.length() ; i++) {
             value=m.get(s.charAt(i));
-            if((i < s.length() - 1&&value<m.get(s.charAt(i+1))){
+            if((i < s.length() - 1&&value<m.get(s.charAt(i+1)))){
                 result-=value;
             }
             else {
