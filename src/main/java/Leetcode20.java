bb38d4dfc733186f7c58df120e0addcc83af9dbc
partner <446729313@qq.com>
2021-08-12 17:35:10 +0800

    更新

--- src/main/java/Leetcode20.java
+++ src/main/java/Leetcode20.java
@@ -3,38 +3,35 @@ import java.util.Stack;
 
 public class Leetcode20 {
     //栈实现匹配括号
-    public Boolean isValid(String s){
+    public Boolean isValid(String s) {
 
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (s.charAt(i) == '['||s.charAt(i)=='{'||s.charAt(i)=='('){
+            if (s.charAt(i) == '[' || s.charAt(i) == '{' || s.charAt(i) == '(') {
                 stack.push(c);
+            } else {
+                if (c == ' ') {
+                    return false;
+                }
+                char topChar = stack.pop();
+                if (c == ')' && topChar != '(') {
+                    return false;
+                }
+                if (c == '}' && topChar != '{') {
+                    return false;
+                }
+
+                if (c == ']' && topChar != '[') {
+                    return false;
+                }
+
+
             }
-            else {
-                if (c == ' '){
-                    return false;
-                }
-                char topChar =stack.pop();
-                if (c == '('&& topChar!=')'){
-                    return false;
-                }
 
 
         }
-
-
-
-
-
-
-
-
-
-
-
-
-        return
+        return stack.isEmpty();
     }
 }
---
+++ src/main/java/bilibiliquestions/ListReverse.java
@@ -0,0 +1,7 @@
+package bilibiliquestions;
+
+/**
+ * @author DELL
+ */
+public class ListReverse {
+}
